738499bf22edb4fd13d1f0b0fafb46843721e308
Dmitry Vinogradov <vinogradov.d.a@ya.ru>
2014-03-28 15:33:10 +0400

    [MOD] homework06/cards.py

--- homework06/cards.py
+++ homework06/cards.py
@@ -24,7 +24,7 @@ class Deck(object):
              'J', 'Q', 'K', 'A')
 
     def __new__(cls):
-        """Singleton"""
+        """ Singleton """
         if not hasattr(cls, 'instance'):
             cls.instance = super(Deck, cls).__new__(cls)
         return cls.instance
@@ -32,13 +32,10 @@ class Deck(object):
     def __init__(self, trump=choice(suits)):
         self.cards = [Card(suit, rank) for suit in self.suits for rank in self.ranks]
 
-        if self.__is_valid_suit(trump):
+        if trump in self.suits:
             self.trump = trump
         else:
             raise Exception('Trump suit is not valid')
-
-    def __is_valid_suit(self, suit):
-        return suit in self.suits
 
     def shuffle(self):
         shuffle(self.cards)
@@ -50,39 +47,58 @@ class Deck(object):
             return [self.cards.pop() for _ in range(amount)]
 
 
-class Player(object):
+class Human(object):
+    @staticmethod
+    def get_quote():
+        return 'Homo homini lupus est'
+
+
+class Person(Human):
     name = None
+    surname = None
+    age = None
+
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+    def __str__(self):
+        return 'Имя: %s\n' \
+               'Фамилия: %s\n' \
+               'Возраст: %d' \
+               % (self.name, self.surname, self.age)
+
+
+class Player(Person):
     hand = []
 
-    def __init__(self, name):
-        self.name = name
-
-    def fill_hand(self, hand):
-        self.hand += hand
+    def fill_hand(self, cards):
+        """ BUG: self.hand += cards """
+        self.hand = self.hand + cards
 
     def get_hand(self):
         return self.hand
+
+
+class Durak():
+    players = None
+
+    def __init__(self, *players):
+        if len(players) != 2:
+            raise Exception('Need two players')
+        else:
+            self.players = players
 
 
 if __name__ == '__main__':
     deck = Deck()
     deck.shuffle()
 
-    alex = Player('Alex')
-    john = Player('John')
+    alex = Player('Dave', 'Grohl', 45)
+    john = Player('John', 'Smith', 50)
 
-    a = deck.get_cards()
-    b = deck.get_cards()
+    alex.fill_hand(deck.get_cards())
+    john.fill_hand(deck.get_cards())
 
-    for card in a:
-        print card.rank, card.suit
-    print
-    for card in b:
-        print card.rank, card.suit
-    print
-
-    alex.fill_hand(a)
-    john.fill_hand(b)
-
-    for card in john.get_hand():
-        print card.rank, card.suit
+    durak = Durak(alex, john)
